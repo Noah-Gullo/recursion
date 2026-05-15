c24e63d8e407d97ccdcf7ba81986dbf5b0eb8f33
Noah Gullo <noahgulloingram@gmail.com>
2026-05-14 20:19:25 -0700

    feat: add fibonacci recursively

--- __tests__/tests.js
+++ __tests__/tests.js
@@ -1,6 +1,6 @@
-import {fibs} from "../src/index.js"
+import {fibs, fibsRec} from "../src/index.js"
 
-test("Iterative base cases when 0 < n < 3", () => {
+test("Iterative cases when 0 < n < 3", () => {
     expect(fibs(1)).toEqual([0]);
     expect(fibs(2)).toEqual([0, 1]);
 })
@@ -10,4 +10,17 @@ test("Iterative case when n > 2", () => {
     expect(fibs(5)).toEqual([0, 1, 1, 2, 3]);
     expect(fibs(6)).toEqual([0, 1, 1, 2, 3, 5]);
     expect(fibs(8)).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
+})
+
+test("Recursive cases when 0 < n < 3", () => {
+    expect(fibsRec(1)).toEqual([0]);
+    expect(fibsRec(2)).toEqual([0, 1]);
+})
+
+test("Recursive case when n > 2", () => {
+    expect(fibsRec(3)).toEqual([0, 1, 1]);
+    expect(fibsRec(5)).toEqual([0, 1, 1, 2, 3]);
+    expect(fibsRec(6)).toEqual([0, 1, 1, 2, 3, 5]);
+    expect(fibsRec(8)).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
+    expect(fibsRec(15)).toEqual([0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]);
 })
--- src/index.js
+++ src/index.js
@@ -11,3 +11,24 @@ export function fibs(num){
 
     return arr;
 }
+
+export function fibsRec(num){
+    if(num <= 0) return [];
+    if(num === 1) return [0];
+    if(num === 2) return [0, 1];
+
+    let arr = [0, 1];
+
+    const recur = function(n, arr){
+        if(n == num){
+            return arr;
+        }else{
+            arr.push(arr[n - 1] + arr[n - 2]);
+            return recur(n + 1, arr);
+        }
+    }
+
+    arr = recur(2, arr);
+
+    return arr;
+}
