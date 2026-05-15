c446011fd52244a4537aa96a76c714e3b18bb365
Noah Gullo <noahgulloingram@gmail.com>
2026-05-14 19:56:41 -0700

    feat: iterative fibonnacci sequence

--- __tests__/tests.js
+++ __tests__/tests.js
@@ -1,5 +1,13 @@
 import {fibs} from "../src/index.js"
 
-test("Normal recursion", () => {
-    expect(fibs(3)).toEqual([]);
+test("Iterative base cases when 0 < n < 3", () => {
+    expect(fibs(1)).toEqual([0]);
+    expect(fibs(2)).toEqual([0, 1]);
+})
+
+test("Iterative case when n > 2", () => {
+    expect(fibs(3)).toEqual([0, 1, 1]);
+    expect(fibs(5)).toEqual([0, 1, 1, 2, 3]);
+    expect(fibs(6)).toEqual([0, 1, 1, 2, 3, 5]);
+    expect(fibs(8)).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
 })
--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-export function fibs(n){
-    return [];
+export function fibs(num){
+    if(num <= 0) return [];
+    if(num === 1) return [0];
+    if(num === 2) return [0, 1];
+
+    let arr = [0, 1];
+
+    for(let i = 2; i < num; i++){
+        arr.push(arr[i - 1] + arr[i - 2]);
+    }
+
+    return arr;
 }
